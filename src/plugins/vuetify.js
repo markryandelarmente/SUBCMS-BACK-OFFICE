cbdc873a0ad804111fef219e2cc044013356b230
Mark Ryan <markryandelarmente104@gmail.com>
2020-08-06 14:19:00 +0800

    feat (style): add polyfill and vuetify global config

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,27 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib';
+import "@mdi/font/css/materialdesignicons.css";
+import Vue from "vue";
+import Vuetify from "vuetify/lib";
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  icons: {
+    iconfont: "mdi",
+  },
+  theme: {
+    dark: false,
+    default: "light",
+    disable: false,
+    themes: {
+      light: {
+        primary: "#243441",
+        secondary: "#424242",
+        accent: "#82B1FF",
+        error: "#FF5252",
+        info: "#2196F3",
+        success: "#4CAF50",
+        warning: "#FB8C00",
+      },
+    },
+  },
 });
