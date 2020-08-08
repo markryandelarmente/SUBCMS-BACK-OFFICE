e98814e3471ddc76e7e6a9bedc8ca909c426419a
Mark Ryan <markryandelarmente104@gmail.com>
2020-08-08 14:17:13 +0800

    feat (webpack): update webpack config, remove duplicate options object

--- webpack.config.js
+++ webpack.config.js
@@ -15,14 +15,6 @@ module.exports = {
             fiber: require("fibers"),
             indentedSyntax: true, // optional
           },
-          // Requires sass-loader@^8.0.0
-          options: {
-            implementation: require("sass"),
-            sassOptions: {
-              fiber: require("fibers"),
-              indentedSyntax: true, // optional
-            },
-          },
         },
       ],
     },
