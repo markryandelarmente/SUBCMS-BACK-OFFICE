909fff64fdded1b367b8cfe4eecd1f15a8370550
Mark Ryan <markryandelarmente104@gmail.com>
2020-08-14 17:24:31 +0800

    feat (component): add _stat.vue to global components

--- src/components/pages/user/index.vue
+++ src/components/pages/user/index.vue
@@ -14,160 +14,174 @@
           </v-tab>
         </v-tabs>
       </v-col>
-      <v-col md="2" class="mt-5">
-        <v-card max-width="350" outlined class="mt-5">
-          <div class="mt-5 ml-5">
-            <v-icon>mdi-account-arrow-right-outline</v-icon>Total
-          </div>
-          <v-list-item three-line>
-            <v-list-item-content class="text-center">
-              <v-list-item-title class="headline">
-                <strong class="display-1">260</strong>
-              </v-list-item-title>
-              <v-list-item-subtitle class="pb-5">Greyhound divisely hello coldly fonwderfully</v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
-        </v-card>
-        <v-card max-width="350" outlined class="mt-5">
-          <div class="mt-5 ml-5">
-            <v-icon>mdi-account-check-outline</v-icon>Active now
-          </div>
-          <v-list-item three-line>
-            <v-list-item-content class="text-center">
-              <v-list-item-title class="headline">
-                <strong class="display-1">180</strong>
-              </v-list-item-title>
-              <v-list-item-subtitle class="pb-5">Greyhound divisely hello coldly fonwderfully</v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
-        </v-card>
-        <v-card max-width="350" outlined class="mt-5">
-          <div class="mt-5 ml-5">
-            <v-icon>mdi-clipboard-alert-outline</v-icon>Pending Tickets
-          </div>
-          <v-list-item three-line>
-            <v-list-item-content class="text-center">
-              <v-list-item-title class="headline">
-                <strong class="display-1">160</strong>
-              </v-list-item-title>
-              <v-list-item-subtitle class="pb-5">Greyhound divisely hello coldly fonwderfully</v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
-        </v-card>
-        <v-card max-width="350" outlined class="mt-5">
-          <div class="mt-5 ml-5">
-            <v-icon>mdi-clipboard-check-outline</v-icon>Solved Tickets
-          </div>
-          <v-list-item three-line>
-            <v-list-item-content class="text-center">
-              <v-list-item-title class="headline">
-                <strong class="display-1">160</strong>
-              </v-list-item-title>
-              <v-list-item-subtitle class="pb-5">Greyhound divisely hello coldly fonwderfully</v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
-        </v-card>
+      <v-col md="2">
+        <Stat :stats="stats" />
       </v-col>
 
-      <v-col md="9" class="text-center">
-        <v-spacer class="mb-5"></v-spacer>
-        <v-data-table
-          light
-          :headers="headers"
-          :items="desserts"
-          :items-per-page="15"
-          :height="620"
-          class="elevation-1"
-        ></v-data-table>
+      <v-col md="9" class="text-center mt-5">
+        <v-card outlined>
+          <v-card-title>
+            <v-icon class="mr-3">mdi-format-list-bulleted</v-icon>Users
+            <v-spacer></v-spacer>
+            <v-spacer></v-spacer>
+            <v-spacer></v-spacer>
+            <v-text-field
+              v-model="filter.keyword"
+              append-icon="mdi-magnify"
+              label="Search"
+              single-line
+              hide-details
+            ></v-text-field>
+          </v-card-title>
+          <v-data-table
+            v-model="selected"
+            :loading="loading"
+            loading-text="Loading. . .Please wait"
+            light
+            :search="filter.keyword"
+            :headers="users.headers"
+            :items="users.data"
+            :server-items-length="total"
+            :items-per-page="filter.count"
+            @page-count="pageCount = $event"
+            show-select
+            hide-default-footer
+            :height="600"
+          >
+            <template v-slot:header.profile_image="{ header }">{{ header.text.toUpperCase() }}</template>
+            <template v-slot:header.email="{ header }">{{ header.text.toUpperCase() }}</template>
+            <template v-slot:header.firstname="{ header }">{{ header.text.toUpperCase() }}</template>
+            <template v-slot:header.lastname="{ header }">{{ header.text.toUpperCase() }}</template>
+            <template v-slot:header.created_at="{ header }">{{ header.text.toUpperCase() }}</template>
+            <template v-slot:header.action="{ header }">{{ header.text.toUpperCase() }}</template>
+            <template v-slot:item.profile_image="{ item }">
+              <v-avatar size="30" class="mx-auto">
+                <img
+                  src="https://jgi.doe.gov/wp-content/uploads/2014/04/Steven_Hallam-slide.jpg"
+                  :alt="item.firstname"
+                />
+              </v-avatar>
+            </template>
+          </v-data-table>
+          <div class="text-center">
+            <v-pagination v-model="filter.page" :length="pageCount" :total-visible="10"></v-pagination>
+          </div>
+        </v-card>
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
+import { USERS_QUERY } from "@/graphql/user.js";
+import Stat from "../../global/_stat";
 export default {
   name: "UserIndex",
+  components: {
+    Stat,
+  },
   data: () => ({
-    headers: [
+    time: 0,
+    loading: true,
+    total: 0,
+    pageCount: 0,
+    selected: [],
+    filter: {
+      role: "user",
+      count: 20,
+      page: 1,
+      order: "asc",
+      column: "created_at",
+      keyword: "",
+    },
+    search: "",
+    users: {
+      headers: [
+        {
+          text: "Profile",
+          align: "start",
+          sortable: false,
+          value: "profile_image",
+        },
+        { text: "Email", value: "email" },
+        { text: "Firstname", value: "firstname" },
+        { text: "Lastname", value: "lastname" },
+        { text: "Registed at", value: "created_at" },
+        { text: "Action", value: "action" },
+      ],
+      data: [],
+    },
+    stats: [
       {
-        text: "Dessert (100g serving)",
-        align: "start",
-        sortable: false,
-        value: "name",
-      },
-      { text: "Calories", value: "calories" },
-      { text: "Fat (g)", value: "fat" },
-      { text: "Carbs (g)", value: "carbs" },
-      { text: "Protein (g)", value: "protein" },
-      { text: "Iron (%)", value: "iron" },
-    ],
-    desserts: [
-      {
-        name: "Frozen Yogurt",
-        calories: 159,
-        fat: 6.0,
-        carbs: 24,
-        protein: 4.0,
-        iron: "1%",
-      },
-      {
-        name: "Ice cream sandwich",
-        calories: 237,
-        fat: 9.0,
-        carbs: 37,
-        protein: 4.3,
-        iron: "1%",
-      },
-      {
-        name: "Eclair",
-        calories: 262,
-        fat: 16.0,
-        carbs: 23,
-        protein: 6.0,
-        iron: "7%",
-      },
-      {
-        name: "Cupcake",
-        calories: 305,
-        fat: 3.7,
-        carbs: 67,
-        protein: 4.3,
-        iron: "8%",
+        icon: "mdi-account-arrow-right-outline",
+        text: "Total",
+        value: "10000",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, cumque?",
       },
       {
-        name: "Gingerbread",
-        calories: 356,
-        fat: 16.0,
-        carbs: 49,
-        protein: 3.9,
-        iron: "16%",
+        icon: "mdi-account-check-outline",
+        text: "Active now",
+        value: "102",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, cumque?",
       },
       {
-        name: "Jelly bean",
-        calories: 375,
-        fat: 0.0,
-        carbs: 94,
-        protein: 0.0,
-        iron: "0%",
+        icon: "mdi-clipboard-alert-outline",
+        text: "Pending Tickets",
+        value: "23",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, cumque?",
       },
       {
-        name: "Lollipop",
-        calories: 392,
-        fat: 0.2,
-        carbs: 98,
-        protein: 0,
-        iron: "2%",
-      },
-      {
-        name: "Honeycomb",
-        calories: 408,
-        fat: 3.2,
-        carbs: 87,
-        protein: 6.5,
-        iron: "45%",
+        icon: "mdi-clipboard-check-outline",
+        text: "Solved Tickets",
+        value: "78",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, cumque?",
       },
     ],
   }),
+  created() {
+    this.getUsers();
+  },
+  methods: {
+    getUsers() {
+      this.$apollo
+        .query({
+          query: USERS_QUERY,
+          variables: { query: { ...this.filter } },
+        })
+        .then(({ data }) => {
+          this.filter.page = data.users.current_page;
+          this.total = data.users.total;
+          this.loading = false;
+          this.users.data = data.users.data.map((user) => {
+            return {
+              ...user,
+              action: "action",
+            };
+          });
+        });
+    },
+  },
+  watch: {
+    "filter.page": function (val) {
+      val;
+      clearTimeout(this.time);
+      this.time = setTimeout(() => {
+        this.getUsers();
+      }, 1000);
+    },
+    "filter.keyword": function (val) {
+      val;
+      clearTimeout(this.time);
+      this.time = setTimeout(() => {
+        this.filter.page = 1;
+        this.getUsers();
+      }, 1000);
+    },
+  },
 };
 </script>
 
