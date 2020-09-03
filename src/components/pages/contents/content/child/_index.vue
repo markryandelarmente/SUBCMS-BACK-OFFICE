3f959805b145c8d39050c2f894ec428644ca7cce
Mark Ryan <markryandelarmente104@gmail.com>
2020-09-03 11:19:26 +0800

    feat (contents): add content index page with route

---
+++ src/components/pages/contents/content/child/_index.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-row>
+    <v-col md="12" sm="12" class="text-center mt-5">_index</v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  data: () => ({}),
+  created() {},
+  methods: {},
+  computed: {},
+  watch: {},
+};
+</script>
+
+<style></style>
---
+++ src/components/pages/contents/content/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-row>
+    <v-col md="12" sm="12" class="text-center mt-5">
+      <h1>Content</h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo incidunt atque esse delectus rem velit, minus repellendus numquam laboriosam suscipit.
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  data: () => ({}),
+  created() {},
+  methods: {},
+  computed: {},
+  watch: {},
+};
+</script>
+
+<style></style>
