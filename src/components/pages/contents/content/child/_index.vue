371385f2767212f384c267331dde9f5631cef02b
Mark Ryan <markryandelarmente104@gmail.com>
2020-09-09 15:33:24 +0800

    feat (contents): add content index and fetching data

--- src/components/pages/contents/content/child/_index.vue
+++ src/components/pages/contents/content/child/_index.vue
@@ -21,54 +21,59 @@
         </v-btn>
       </v-toolbar>
     </v-col>
-    <v-col v-for="n in 10" :key="n" class="d-flex child-flex" md="2" sm="6">
-      <v-card elevation="1" max-width="400">
-        <v-img
-          class="grey lighten-2"
-          height="200px"
-          :src="`https://picsum.photos/500/300?image=${n * 5 + 10}`"
-          :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}`"
-          aspect-ratio="1"
-        >
-          <template v-slot:placeholder>
-            <v-row class="fill-height ma-0" align="center" justify="center">
-              <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
-            </v-row>
-          </template>
-        </v-img>
+    <v-col cols="12">
+      <v-row>
+        <v-col v-for="(content, index) in contents.data" :key="index" md="2" sm="6" xs="12">
+          <v-card elevation="1" max-width="380">
+            <v-img
+              class="grey lighten-2"
+              height="200px"
+              :src="content.image.url"
+              :lazy-src="content.image.url"
+              aspect-ratio="1"
+            >
+              <template v-slot:placeholder>
+                <v-row class="fill-height ma-0" align="center" justify="center">
+                  <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
+                </v-row>
+              </template>
+            </v-img>
 
-        <v-card-actions class="pb-0">
-          <v-list-item class="list-item">
-            <v-card-subtitle class="pl-0">
-              <v-icon small>mdi-heart-outline</v-icon>&nbsp; 200
-              <span class="mx-2"></span>
-              <v-icon small>mdi-message-outline</v-icon>&nbsp; 16
-              <span class="mx-2"></span>
-              <v-icon small>mdi-eye-outline</v-icon>&nbsp; 16K
-            </v-card-subtitle>
+            <v-card-actions class="pb-0">
+              <v-list-item class="list-item">
+                <v-card-subtitle class="pl-0">
+                  <v-icon small>mdi-heart-outline</v-icon>&nbsp; 200
+                  <span class="mx-2"></span>
+                  <v-icon small>mdi-message-outline</v-icon>&nbsp; 16
+                  <span class="mx-2"></span>
+                  <v-icon small>mdi-eye-outline</v-icon>&nbsp; 16K
+                </v-card-subtitle>
 
-            <v-row justify="end">
-              <span class="mr-1">·</span>
-              <v-icon @click="alarm" small class="mr-1">mdi-dots-vertical</v-icon>
-            </v-row>
-          </v-list-item>
-        </v-card-actions>
+                <v-row justify="end">
+                  <span class="mr-1">·</span>
+                  <v-icon @click="alarm" small class="mr-1">mdi-dots-vertical</v-icon>
+                </v-row>
+              </v-list-item>
+            </v-card-actions>
 
-        <v-card-text class="text--secondary pt-0">
-          <div class="font-weight-medium">Upper body workout</div>
+            <v-card-text class="text--secondary pt-0">
+              <div class="font-weight-medium">{{ content.title }}</div>
 
-          <div class="font-weight-light">Lorem ipsum dolor sit amet consectetur ...</div>
-          <div class="font-weight-thin">2months ago</div>
-        </v-card-text>
+              <div class="font-weight-light">{{ content.description }} ...</div>
+              <div class="font-weight-thin">2months ago</div>
+            </v-card-text>
 
-        <v-card-actions>
-          <v-chip outlined>
-            <v-icon small>mdi-video-outline</v-icon>
-            <span class="subtitle-2 font-weight-light ml-1">Program</span>
-          </v-chip>
-        </v-card-actions>
-      </v-card>
+            <v-card-actions>
+              <v-chip outlined>
+                <v-icon small>mdi-video-outline</v-icon>
+                <span class="subtitle-2 font-weight-light ml-1">{{ content.content_type.name }}</span>
+              </v-chip>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
     </v-col>
+
     <v-dialog v-model="content_type.dialog" scrollable max-width="400px">
       <v-card>
         <v-card-title>Select type of content</v-card-title>
@@ -95,6 +100,7 @@
 </template>
 
 <script>
+import { CONTENTS_QUERY } from "@/graphql/content.js";
 export default {
   data: () => ({
     categories: [
@@ -219,15 +225,48 @@ export default {
         },
       ],
     },
+    contents: {
+      filter: {
+        type: "",
+        count: 12,
+        page: 1,
+        order: "desc",
+        column: "created_at",
+        keyword: "",
+      },
+      loading: false,
+      total: 0,
+      pageCount: 0,
+      data: [],
+    },
   }),
-  created() {},
+  created() {
+    this.fetchData();
+  },
   methods: {
     alarm() {},
     selectType() {
       console.log("hey");
     },
+    fetchData() {
+      this.$apollo
+        .query({
+          query: CONTENTS_QUERY,
+          variables: { query: { ...this.computedFilter } },
+        })
+        .then(({ data }) => {
+          this.contents.filter.page = data.contents.current_page;
+          this.contents.total = data.contents.total;
+          this.contents.loading = false;
+          this.contents.data = data.contents.data;
+        });
+    },
   },
-  computed: {},
+  computed: {
+    computedFilter: function () {
+      return Object.assign({}, this.contents.filter);
+    },
+  },
   watch: {},
 };
 </script>
