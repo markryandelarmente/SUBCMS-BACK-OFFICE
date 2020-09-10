e0b1d632f8ae07beb1546934b7409813fd7f7bbb
Mark Ryan <markryandelarmente104@gmail.com>
2020-09-10 15:04:02 +0800

    feat (contents): add structure to contents form

---
+++ src/components/pages/contents/content/child/forms/_article.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-col md="11" class="p-0">
+    <h1>ARTICLE FORM HERE</h1>
+  </v-col>
+</template>
+
+<script>
+export default {
+  data: () => ({}),
+  methods: {},
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/pages/contents/content/child/forms/_video.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-col md="11" class="p-0">
+    <h1>VIDEO FORM HERE</h1>
+  </v-col>
+</template>
+
+<script>
+export default {
+  data: () => ({}),
+  methods: {},
+};
+</script>
+
+<style>
+</style>
