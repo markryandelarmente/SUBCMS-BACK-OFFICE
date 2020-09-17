91e4c83b1a65e952e7074b58f32fff7eaff29dbe
Mark Ryan <markryandelarmente104@gmail.com>
2020-09-17 10:49:57 +0800

    feat (contents): add categories fetchData in index page

--- src/graphql/category.js
+++ src/graphql/category.js
@@ -9,4 +9,20 @@ const CATEGORIES_QUERY = gql`
   }
 `;
 
-export { CATEGORIES_QUERY };
+const CATEGORIES_PAGINATED_QUERY = gql`
+  query categories($query: DefaultFilter) {
+    categories(query: $query) {
+      total
+      current_page
+      data {
+        id
+        name
+        image {
+          url
+        }
+      }
+    }
+  }
+`;
+
+export { CATEGORIES_QUERY, CATEGORIES_PAGINATED_QUERY };
